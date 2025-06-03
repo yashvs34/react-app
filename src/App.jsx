37519579fe034b56eb79c4d7414fb70f21c43bdf
Yash <yashvardhansingh232@gmail.com>
2025-06-04 01:40:15 +0530

    Added Loading functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import Loading from './components/Loading';
+import Footer from './components/Footer';
 import './App.css'
+import axios from "axios"
+import {BrowserRouter, Route} from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [data, setData] = useState([]);
 
-  return (
+  useEffect(() => {
+    axios.get(`https://dummyjson.com/products?limit=${10}&skip=0`)
+      .then((response) => {
+        setData(response.data.products);
+      })
+  }, []);
+
+  return data.length === 0 ? <Loading/> : (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        hi
+      </BrowserRouter>
+
+      <Footer />
     </>
   )
+
 }
 
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer ()
+{
+    return (
+        <div className="footer">
+            hi
+        </div>
+    )
+}
+
+export default Footer
