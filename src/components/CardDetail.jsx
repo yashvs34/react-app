d550a80796484c15c8e3722b2cc1d444fbe5c957
Yash <yashvardhansingh232@gmail.com>
2025-06-05 00:56:28 +0530

    Added basic structure of details page

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -20,20 +20,65 @@ function CardDetail ()
         index++;
     }
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState();
 
     useEffect(() => {
         axios.get(`https://dummyjson.com/products?limit=1&skip=${id-1}`)
           .then((response) => {
             setData(response.data.products[0]);
+            console.log(response.data.products[0]);
           })
     }, []);
 
     return (
         <>
-            {data.length === 0 ? <Loading/> : 
-                <div>
-                    hi
+            {!data ? <Loading/> : 
+                <div className="detail-card">
+                    <img src={`${data.thumbnail}`} alt="thumbnail" />
+                    <div>
+                        {data.title}
+                    </div>
+                    <div>
+                        {data.description}
+                    </div>
+                    <div>
+                        {data.category}
+                    </div>
+                    <div>
+                        {data.price}
+                    </div>
+                    <div>
+                        {data.discountPercentage}
+                    </div>
+                    <div>
+                        {data.rating}
+                    </div>
+                    <div>
+                        {data.stock}
+                    </div>
+                    <div>
+                        {data.tags.map((item) => {
+                            return <>{item}</>
+                        })}
+                    </div>
+                    <div>
+                        {data.brand}
+                    </div>
+                    <div>
+                        {data.sku}
+                    </div>
+                    <div>
+                        {data.weight}
+                    </div>
+                    <div>
+                        {data.warrantyInformation}
+                    </div>
+                    <div>
+                        {data.shippingInformation}
+                    </div>
+                    <div>
+                        {data.availabilityStatus}
+                    </div>
                 </div>
             }
         </>
